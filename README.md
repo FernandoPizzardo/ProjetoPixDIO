aee10d6697d4fce968be45febba1020ccee58246
fernando pizzardo <fermelo95@gmail.com>
2021-02-11 20:21:48 -0300

    Projeto Pix

---
+++ README.md
@@ -0,0 +1,5 @@
+# Digital Innovation One - Prática .NET
+
+## POO na prática: criando uma aplicação
+
+# Variação feita por Fernando Melo Pizzardo
---
+++ enum/TipoTransferencia.cs
@@ -0,0 +1,10 @@
+namespace DIO.Bank
+{
+   
+    public enum TipoTransferencia
+    {
+        DOC = 1,
+        TED = 2,
+        Pix = 3 
+    }
+}
